a28bd391e9060905ec0a2b7a62b5aaf99dd8e88e
fathur13 <91817157+fathur13@users.noreply.github.com>
2021-12-20 08:16:42 +0700

    Initial commit

---
+++ system/resources/views/template/admin/section/assets.blade.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script src="{{ url('public') }}/assets/js/jquery.js"></script>
+  <script src="{{ url('public') }}/assets/js/jquery.glide.js"></script>
+
+  <link rel="stylesheet" href="{{ url('public') }}/assets/css/style.css">
+  <link rel="stylesheet" href="{{ url('public') }}/assets/css/animate.css">
+  <script type="text/javascript" scr="{{ url('public') }}/assets/js/MyJQ.js"></script>
+  <script src="{{ url('public') }}/assets/js/jquery.localScroll.min.js" type="text/javascript"></script>
+  <script src="{{ url('public') }}/assets/js/jquery.scrollTo.min.js" type="text/javascript"></script>
+  <script src="{{ url('public') }}/assets/js/wow.min.js" type="text/javascript"></script>
+
+  <title>
+    Admin
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="{{url('public')}}/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="{{url('public')}}/assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="{{url('public')}}/assets/demo/demo.css" rel="stylesheet" />
---
+++ system/resources/views/template/admin/section/sidebar.blade.php
@@ -0,0 +1,48 @@
+ <div class="sidebar-wrapper">
+        <div class="logo">
+          <a href="javascript:void(0)" class="simple-text logo-mini">
+            FH
+          </a>
+          <a href="javascript:void(0)" class="simple-text logo-normal">
+            Tole Team
+          </a>
+        </div>
+        <ul class="nav">
+          <li class="active ">
+            <a href="{{url('admin')}}/dashboard">
+              <i class="tim-icons icon-chart-pie-36"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/article">
+              <i class="tim-icons icon-atom"></i>
+              <p>Artikel</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('home')}}">
+              <i class="tim-icons icon-atom"></i>
+              <p>Blog</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/coment">
+              <i class="tim-icons icon-bell-55"></i>
+              <p>Komentar</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('admin')}}/user">
+              <i class="tim-icons icon-single-02"></i>
+              <p>User</p>
+            </a>
+          </li>
+          <li>
+            <a href="{{url('login')}}">
+              <i class="tim-icons icon-single-02"></i>
+              <p>Log Out</p>
+            </a>
+          </li>
+        </ul>
+      </div>
